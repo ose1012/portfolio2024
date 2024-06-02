47e900eccad0cee65f958414c226e85da8256c61
Oh seoung eun <nnmmaz95@gmail.com>
2024-06-03 02:04:18 +0900

    commit

---
+++ portfolio/src/Components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Text } from "@mantine/core";
+import "./Profile.css";
+
+interface ProfileProps {
+  name: string;
+  profileImage: string;
+}
+
+const Profile: React.FC<ProfileProps> = ({ name, profileImage }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignContent: "flex-start",
+      }}
+    >
+      <img
+        src={profileImage}
+        style={{ width: "250px", borderRadius: "50%" }}
+        className="profile-image"
+      />
+      <Text>{name}</Text>
+    </div>
+  );
+};
+
+export default Profile;
